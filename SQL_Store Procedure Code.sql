4704bcfb1cf955875d95feac7a7842cf4ec5ec68
Arjun-rai-k <arjun.rai594@gmail.com>
2025-08-30 10:41:31 +0530

    Update SQL_Store Procedure Code.sql

--- SQL_Store Procedure Code.sql
+++ SQL_Store Procedure Code.sql
@@ -1,4 +1,4 @@
-
+1)
 -- Create table
 CREATE TABLE Test_proc (
     Id INT,
@@ -20,4 +20,30 @@ CALL Insert_test(2, 'Vikram');
 -- Check inserted data
 SELECT * FROM Test_proc;
 
+2)
+CREATE TABLE EMPLOYEES (
+EMPID SERIAL PRIMARY KEY,
+EMPNAME VARCHAR(100),
+DEPT VARCHAR(100),
+SALARY INT
+)
+
+CREATE OR REPLACE PROCEDURE ADDEMPLOYEE(p_name VARCHAR,p_dept VARCHAR,p_salary INT)
+Language plpgsql
+AS $$
+BEGIN 
+
+INSERT INTO EMPLOYEES (EMPNAME,DEPT,SALARY)
+VALUES (p_name,p_dept,p_salary);
+END;
+$$
+
+--call it
+CALL ADDEMPLOYEE ('Arjun', 'IT', 60000);
+CALL ADDEMPLOYEE ('Vikram', 'Finance', 75000)
+
+--verify 
+Select * from EMPLOYEES
+
+3)
 
